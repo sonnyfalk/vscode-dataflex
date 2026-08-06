a500ebf9b992d86c490c7eb9c8a0309cf8350bbe
Sonny Falk <sonnyfalk@gmail.com>
2026-08-05 21:22:07 -0700

    Add custom LSP client side request to apply edits while preserving selection.
    This is used when the LSP server tries to auto-close scope on the last line of the file, e.g. Object/End_Object.

--- out/extension.js
+++ out/extension.js
@@ -37,6 +37,43 @@ exports.activate = activate;
 exports.deactivate = deactivate;
 const vscode = __importStar(require("vscode"));
 const node_1 = require("vscode-languageclient/node");
+class ApplyEditPreserveSelection {
+    client;
+    constructor(client) {
+        this.client = client;
+    }
+    fillClientCapabilities(capabilities) {
+        if (!capabilities.experimental) {
+            capabilities.experimental = {};
+        }
+        capabilities.experimental.dataFlexApplyEditPreserveSelection = true;
+    }
+    getState() {
+        return { kind: 'static' };
+    }
+    initialize(capabilities) {
+        this.client.onRequest("dataFlex/applyEditPreserveSelection", async (payload) => {
+            const { textDocument, edits } = payload;
+            const editor = vscode.window.visibleTextEditors.find(e => e.document.uri.toString() === textDocument.uri);
+            if (!editor)
+                return false;
+            if (textDocument.version != null && editor.document.version !== textDocument.version) {
+                return false;
+            }
+            const savedSelections = editor.selections;
+            const success = await editor.edit(editBuilder => {
+                for (const edit of edits) {
+                    const range = new vscode.Range(edit.range.start.line, edit.range.start.character, edit.range.end.line, edit.range.end.character);
+                    editBuilder.replace(range, edit.newText);
+                }
+            });
+            editor.selections = savedSelections;
+            return success;
+        });
+    }
+    clear() {
+    }
+}
 // This method is called when your extension is activated
 function activate(context) {
     const path = vscode.Uri.joinPath(context.extensionUri, "server", "dataflex-lsp");
@@ -49,6 +86,7 @@ function activate(context) {
         documentSelector: [{ scheme: "file", language: "dataflex" }],
     };
     let client = new node_1.LanguageClient("DataFlex Language Server", serverOptions, clientOptions);
+    client.registerFeature(new ApplyEditPreserveSelection(client));
     client.start();
 }
 // This method is called when your extension is deactivated
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,60 @@
 import * as vscode from "vscode";
 import {
   Command,
+  ClientCapabilities,
   Executable,
+  FeatureState,
   LanguageClient,
   LanguageClientOptions,
+  ServerCapabilities,
   ServerOptions,
+  StaticFeature,
   TransportKind,
 } from "vscode-languageclient/node";
+
+class ApplyEditPreserveSelection implements StaticFeature {
+  constructor(private client: LanguageClient) { }
+
+  fillClientCapabilities(capabilities: ClientCapabilities): void {
+    if (!capabilities.experimental) {
+      capabilities.experimental = {};
+    }
+    capabilities.experimental.dataFlexApplyEditPreserveSelection = true;
+  }
+
+  getState(): FeatureState {
+    return { kind: 'static' };
+  }
+
+  initialize(capabilities: ServerCapabilities): void {
+    this.client.onRequest("dataFlex/applyEditPreserveSelection", async (payload) => {
+      const { textDocument, edits } = payload;
+      const editor = vscode.window.visibleTextEditors.find(e => e.document.uri.toString() === textDocument.uri);
+      if (!editor) return false;
+
+      if (textDocument.version != null && editor.document.version !== textDocument.version) {
+        return false;
+      }
+
+      const savedSelections = editor.selections;
+      const success = await editor.edit(editBuilder => {
+        for (const edit of edits) {
+          const range = new vscode.Range(
+            edit.range.start.line, edit.range.start.character,
+            edit.range.end.line, edit.range.end.character
+          );
+          editBuilder.replace(range, edit.newText);
+        }
+      });
+      editor.selections = savedSelections;
+
+      return success;
+    });
+  }
+
+  clear(): void {
+  }
+}
 
 // This method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
@@ -31,6 +79,8 @@ export function activate(context: vscode.ExtensionContext) {
     serverOptions,
     clientOptions,
   );
+
+  client.registerFeature(new ApplyEditPreserveSelection(client));
   client.start();
 }
 
